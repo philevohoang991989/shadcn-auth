599742d3caf2cf40322af89d1219ed5062795370
phi.le <phi.le@bnksolution.com>
2024-04-26 15:27:40 +0700

    update code

--- lib/hook/useAxiosAuth.ts
+++ lib/hook/useAxiosAuth.ts
@@ -1,32 +1,44 @@
-"use client"
+"use client";
 
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { axiosAuth } from "../axios";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 
-const useAxiosAuth = () =>{
-    const {data: session} = useSession();
-    useEffect(()=>{
-        const requestIntercept = axiosAuth.interceptors.request.use((config)=>{
-            if(!config.headers["Authorization"]){
-                config.headers["Authorization"] = `Bearer ${session?.token}`
-            }
-            return config;
-        });
-        const responseIntercept = axiosAuth.interceptors.response.use(function (response) {
-            // Any status code that lie within the range of 2xx cause this function to trigger
-            // Do something with response data
-            return response.data;
-          }, function (error) {
-            // Any status codes that falls outside the range of 2xx cause this function to trigger
-            // Do something with response error
-            return Promise.reject(error);
-          });
-        return ()=>{
-            axiosAuth.interceptors.request.eject(requestIntercept)
-            axiosAuth.interceptors.response.eject(responseIntercept)
+const useAxiosAuth = () => {
+  const { toast } = useToast();
+  const { data: session } = useSession();
+  useEffect(() => {
+    const requestIntercept = axiosAuth.interceptors.request.use((config) => {
+      if (!config.headers["Authorization"]) {
+        config.headers["Authorization"] = `Bearer ${session?.token}`;
+      }
+      return config;
+    });
+    const responseIntercept = axiosAuth.interceptors.response.use(
+      function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response.data;
+      },
+      async function (error) {
+        const prevRequest = error?.config;
+        if (error?.response?.status === 401 && !prevRequest?.sent) {
+          toast({
+            title: "Un authenticated",
+            description: "Un authenticated",
+          
+          })
         }
-    },[session])
-    return axiosAuth
-}
+        return Promise.reject(error);
+      }
+    );
+    return () => {
+      axiosAuth.interceptors.request.eject(requestIntercept);
+      axiosAuth.interceptors.response.eject(responseIntercept);
+    };
+  }, [session]);
+  return axiosAuth;
+};
 export default useAxiosAuth;
